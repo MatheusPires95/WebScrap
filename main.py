a8829aac23422716dadbc175046ee823087185d6
MatheusPires95 <macp0411@gmail.com>
2020-02-17 00:49:59 -0300

    some more modifications

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ req = requests.get('https://www.basketball-reference.com/leagues/NBA_2018_totals
 if req.status_code == 200:
     print('Requisição bem sucedida!')
     content = req.content
+
+soup = BeautifulSoup(content, 'html.parser')
+table = soup.find(name='table')
+
+table_str = soup.find(name='table', attrs={'id':'confs_standings_W'})
+df = pd.read_html(table_str))[0]
